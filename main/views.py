27e7b140c5655e2c425115d6cf9c228b93fd32c1
Johesive <c.joh101508@gmail.com>
2026-03-16 19:30:04 +0800

    server original version

---
+++ main/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.template import loader
+from django.conf import settings
+from django import forms
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
+from django.views.static import serve
+
+import os
+import json
+
+def index(request):
+    template = loader.get_template('index.html')
+    print(settings.STATICFILES_DIRS[0])
+    static_files = list(filter(lambda x: not x.startswith("."), os.listdir(os.path.join(str(settings.STATICFILES_DIRS[0]), 'files'))))
+    return HttpResponse(template.render({
+        "version": "0.0.2",
+        "file_list": static_files
+    }, request))
+
+def docs(request):
+    template = loader.get_template('docs.html')
+    return HttpResponse(template.render({"version": "0.0.2"}, request))
